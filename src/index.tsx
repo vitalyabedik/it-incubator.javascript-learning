7eccb500e6b9a32878c775d29507379040a7e5e4
Vitali Bedik <vitalya.bedik@gmail.com>
2023-04-03 00:04:05 +0300

    feat: finish 01-lesson

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {splitIntoWords} from './01-hello-tests/01';
+
+const sentense = 'Hello my friends!'
+
+const result = splitIntoWords(sentense)
+
+console.log(result)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
